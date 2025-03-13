f12e370d60e612cd71678c30db4760908b369bd0
pedro <pedro.hlgoncalves@senacsp.edu.br>
2025-03-13 19:57:09 -0300

    Criação dos métodos no dm

---
+++ src/filme/filme.dm.ts
@@ -0,0 +1,57 @@
+import { Injectable } from "@nestjs/common";
+import { FilmeEntity } from "./filme.entity";
+
+@Injectable()
+export class FilmesArmazenados {
+    #filmes: FilmeEntity[] = [
+        { id: '1', nome: 'Tropa de Elite', duracao: 135, sinopse: 'Historia de um capitão do BOPE', ano: '2015', genero: 'policial/drama' }
+    ];
+
+    AficionarFilme(filme: FilmeEntity) {
+        this.#filmes.push(filme)
+    }
+
+    get Filmes() {
+        return this.#filmes
+    }
+
+    atualizaFilme(id: string, dadosAtualizacao: Partial<FilmeEntity>) {
+        const filme = this.buscaPorID(id);
+
+        Object.entries(dadosAtualizacao).forEach(
+            ([chave, valor]) => {
+                if (chave === 'id') {
+                    return
+                }
+                if (valor === undefined) {
+                    return
+                }
+
+                filme[chave] = valor
+            }
+        )
+
+        return filme
+    }
+
+    private buscaPorID(id: string) {
+        const possivelFilme = this.#filmes.find(
+            filmeSalvo => filmeSalvo.id === id
+        )
+
+        if (!possivelFilme) {
+            throw new Error('Filme não encontrado')
+        }
+        return possivelFilme;
+    }
+
+    removeFilme(id: string) {
+        const filme = this.buscaPorID(id);
+
+        this.#filmes = this.#filmes.filter(
+            filmeSalvo => filmeSalvo.id === id
+        )
+
+        return filme
+    }
+}
